c32adbd220947ba26be3ab73b18bfffffd0806e4
Michael Goh <mikegoh1997@gmail.com>
2025-08-21 13:45:01 -0700

    Add webpage

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+export default function Home() {
+	return (
+	  <>
+		<style jsx global>{`
+		  body {
+			background-color: black;
+			color: white;
+			margin: 0;
+			height: 100%;
+		  }
+		  h1 {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 100vh;
+			margin: 0;
+		  }
+		`}</style>
+		<h1>Hello, this is a page for the TLP Bot on Discord!</h1>
+	  </>
+	);
+  }
+  
